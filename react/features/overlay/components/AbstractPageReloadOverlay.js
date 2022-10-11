fcd6a27a070b05fea7bb5c956125d15915e2d8a6
M. Fahim Jubayer <fahimjubayer.dsi@gmail.com>
2022-10-11 13:13:03 +0600

    SSF-451: Change jitsi meet re-connection timeout to 30 seconds

--- react/features/overlay/components/AbstractPageReloadOverlay.js
+++ react/features/overlay/components/AbstractPageReloadOverlay.js
@@ -148,7 +148,7 @@ export default class AbstractPageReloadOverlay<P: Props>
          *
          * @type {number}
          */
-        const timeoutSeconds = 10 + randomInt(0, 20);
+        const timeoutSeconds = 30;
 
         let message, title;
 
